e20e5d379db0a81580ab848580b2fecfccffea57
Nurmuhammadali <nurmuhammadali0709@gmail.com>
2021-10-07 17:27:12 +0500

    commit

--- accaunts/admin.py
+++ accaunts/admin.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import Group
 from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('user_fish', 'phone_number', 'online_auditoriya', 'offline_zoom')
+    list_display = ('user_fish', 'phone_number', 'offline_auditoriya', 'online_zoom')
 
 admin.site.unregister(Group)
 admin.site.site_header = 'islom-moliyasi uz | admin'
