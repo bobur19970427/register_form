2c77c854b85f27122160b46158cc4b42d0f51ee6
Nurmuhammadali <61279351+Nurmuhammadali@users.noreply.github.com>
2021-10-07 16:20:07 +0500

    Update settings.py

--- register_form/settings.py
+++ register_form/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4vqwy3-=n9$is$v4xd#h*igpl=1w(lsy3npes=ll&tmprj8-gc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
